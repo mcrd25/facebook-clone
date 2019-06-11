70b9c7eb1bff868ecf8dd95ecc75700e4b82bde6
mcrd25 <mayacrd25@gmail.com>
2019-06-11 17:52:15 -0400

    updated tests for attribute presence in User model and added necessary migrations to make test pass

--- fbclone/spec/factories/users.rb
+++ fbclone/spec/factories/users.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  birth_date             :date
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  gender                 :string
+#  last_name              :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 FactoryBot.define do
   factory :user do
     
--- fbclone/spec/models/user_spec.rb
+++ fbclone/spec/models/user_spec.rb
@@ -1,57 +1,90 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  birth_date             :date
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  gender                 :string
+#  last_name              :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.new }
 
-  describe 'general expected attributes' do 
-    it 'should include the :first_name attribute' do 
-      expect(user.attributes).to include(:first_name)
+  describe 'test for presence of model attributes for' do
+
+    describe 'general expected attributes' do 
+      it 'should include the :first_name attribute' do 
+        expect(user.attributes).to include('first_name')
+      end
+
+      it 'should include the :last_name attribute' do 
+        expect(user.attributes).to include('last_name')
+      end
+
+      it 'should include the :email attribute' do 
+        expect(user.attributes).to include('email')
+      end
+
+      it 'should include the :gender attribute' do 
+        expect(user.attributes).to include('gender')
+      end
+
+      it 'should include the :birthdate attribute' do 
+        expect(user.attributes).to include('birth_date')
+      end
+    end 
+
+    describe 'Devise specific attributes' do 
+      it 'should include the :encrypted_password attribute' do 
+        expect(user.attributes).to include('encrypted_password')
+      end
+
+      it 'should include the :reset_password_sent_at attribute' do 
+        expect(user.attributes).to include('reset_password_sent_at')
+      end
+
+      it 'should include the :reset_password_token attribute' do 
+        expect(user.attributes).to include('reset_password_token')
+      end
+
+      it 'should include the :remember_created_at attribute' do 
+        expect(user.attributes).to include('remember_created_at')
+      end
     end
 
-    it 'should include the :last_name attribute' do 
-      expect(user.attributes).to include(:last_name)
-    end
+    describe 'Rails specific attributes' do
+      it 'should include the :created_at attribute' do 
+        expect(user.attributes).to include('created_at')
+      end
 
-    it 'should include the :email attribute' do 
-      expect(user.attributes).to include(:email)
-    end
+      it 'should include the :updated_at attribute' do 
+        expect(user.attributes).to include('updated_at')
+      end
 
-    it 'should include the :gender attribute' do 
-      expect(user.attributes).to include(:gender)
-    end
-
-    it 'should include the :birthdate attribute' do 
-      expect(user.attributes).to include(:birthdate)
-    end
-
-    it 'should include the :created_at attribute' do 
-      expect(user.attributes).to include(:created_at)
-    end
-
-    it 'should include the :updated_at attribute' do 
-      expect(user.attributes).to include(:updated_at)
-    end
-
-    it 'should include the :id attribute' do 
-      expect(user.attributes).to include(:id)
-    end
-  end 
-
-  describe 'Devise specific attributes' do 
-    it 'should include the :encrypted_password attribute' do 
-      expect(user.attributes).to include(:encrypted_password)
-    end
-
-    it 'should include the :reset_password_sent_at attribute' do 
-      expect(user.attributes).to include(:reset_password_sent_at)
-    end
-
-    it 'should include the :reset_password_token attribute' do 
-      expect(user.attributes).to include(:reset_password_token)
-    end
-
-    it 'should include the :remember_created_at attribute' do 
-      expect(user.attributes).to include(:remember_created_at)
+      it 'should include the :id attribute' do 
+        expect(user.attributes).to include('id')
+      end
     end
   end
+
+  describe 'Basic validations' do
+
+  end
+
 end
