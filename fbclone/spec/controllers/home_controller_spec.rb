7789f1e1ce198ca3d60ef6a61f6410f5f642bc89
Adriaan Beiertz <adriaanbd@gmail.com>
2019-06-24 18:31:38 -0500

    added new gem, updated sessions tests

--- fbclone/spec/controllers/home_controller_spec.rb
+++ fbclone/spec/controllers/home_controller_spec.rb
@@ -4,18 +4,27 @@ RSpec.describe HomeController, type: :controller do
 
 	describe 'Index tests' do
 
+		before do 
+			@user = FactoryBot.create(:user)
+		end
+
 		context 'response' do
 		  it 'expects response to index to be successful' do 
+		    sign_in @user
 		    get :index 
 		    expect(response).to be_success
 		  end
 		end
+
 		context 'http status' do
 		  it 'returns a 200 response' do
+		    sign_in @user
 		    get :index
 		    expect(response).to have_http_status '200'
 		  end
+
 		  it 'does not return 404 response' do
+		  	sign_in @user
 		  	get :index
 		  	expect(response).to_not have_http_status '404'
 		  end
@@ -24,7 +33,7 @@ RSpec.describe HomeController, type: :controller do
 		context 'session' do
 			it 'renders registration page when user is not signed in' do 
 				get :index 
-				expect(response).to render_template(new_user_registration_path)
+				expect(response).to render_template(:new)
 			end 
 
 			it 'renders new_controller#index when user is signed in' do
