da5d71e0dc6c4e4dde9b0318f367168407d8eae0
mcrd25 <mayacrd25@gmail.com>
2019-06-25 19:32:05 -0400

    Modified index method for home controller

--- fbclone/app/controllers/home_controller.rb
+++ fbclone/app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
 
-  # before_action :authenticate_user!
-
   def index
-  	if !user_signed_in? 
-  		render 'new'
-  	end
+  	render :new if !user_signed_in?
   end
+
 end
