b94f6f9af09853cc4bbed96e17eb2e0bbf2d7a33
mcrd25 <mayacrd25@gmail.com>
2019-06-24 20:23:19 -0400

    updated controller to render new view if user not signed in

--- fbclone/app/controllers/home_controller.rb
+++ fbclone/app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
+  	if !user_signed_in? 
+  		render 'new'
+  	end
   end
 end
