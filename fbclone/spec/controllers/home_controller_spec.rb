ab1b4012ac462a0069df536ed0110b443caf5b4a
Adriaan Beiertz <adriaanbd@gmail.com>
2019-06-25 18:54:22 -0500

    updated home controller spec

--- fbclone/spec/controllers/home_controller_spec.rb
+++ fbclone/spec/controllers/home_controller_spec.rb
@@ -2,44 +2,43 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-	describe 'Index tests' do
+	describe 'GET index' do
 
 		before do 
 			@user = FactoryBot.create(:user)
 		end
 
-		context 'response' do
-		  it 'expects response to index to be successful' do 
+		context 'when user is logged in' do 
+			it 'responds succesfully' do 
 		    sign_in @user
 		    get :index 
 		    expect(response).to be_success
 		  end
-		end
 
-		context 'http status' do
-		  it 'returns a 200 response' do
+		  it 'responds with 200' do
 		    sign_in @user
 		    get :index
 		    expect(response).to have_http_status '200'
 		  end
 
-		  it 'does not return 404 response' do
+		  it 'does not respond with 404' do
 		  	sign_in @user
 		  	get :index
 		  	expect(response).to_not have_http_status '404'
 		  end
-		end
 
-		context 'session' do
-			it 'renders registration page when user is not signed in' do 
-				get :index 
-				expect(response).to render_template(:new)
-			end 
-
-			it 'renders new_controller#index when user is signed in' do
+		  it 'renders index' do
 				sign_in @user 
 				get :index
 				expect(response).to render_template(:index)
+			end 
+
+		end
+
+		context 'when user is not logged in' do
+			it 'renders new' do 
+				get :index 
+				expect(response).to render_template(:new)
 			end 
 		end
 	end
