2563851f2d2ac7e4b9be7fe04c20393f1a412e50
Adriaan Beiertz <adriaanbd@gmail.com>
2019-06-27 19:40:59 -0500

    added more edit profile specs and updated profile#edit

--- fbclone/app/controllers/profiles_controller.rb
+++ fbclone/app/controllers/profiles_controller.rb
@@ -1,13 +1,16 @@
 class ProfilesController < ApplicationController
 	before_action :set_user
+
   def show 
   end
 
   def edit
-    redirect_to  action: 'show', username: params[:username] if !user_signed_in?
+    redirect_to  action: 'show', 
+                 username: params[:username] if current_user != @user
   end
 
   private
+
   def set_user
   	@user = User.find_by(username: params[:username])
   end
--- fbclone/spec/controllers/profiles_controller_spec.rb
+++ fbclone/spec/controllers/profiles_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ProfilesController, type: :controller do
 
   before do 
         @user = FactoryBot.create(:user)
+        @other = FactoryBot.create(:user)
       end
 
   describe 'GET show' do 
@@ -39,23 +40,38 @@ RSpec.describe ProfilesController, type: :controller do
 
   describe 'GET edit' do 
     context 'when user is logged in' do
+
+      before do 
+        sign_in @user
+      end
+
       it 'responds succesfully' do 
-        sign_in @user 
         get :edit, params: { username: @user.username }
         expect(response).to be_successful
       end
 
       it 'responds succesfully' do 
-        sign_in @user 
         get :edit, params: { username: @user.username }
         expect(response).to have_http_status '200'
+      end
+
+      it 'renders :edit when current_user == params[username]' do 
+        get :edit, params: { username: @user.username }
+        expect(response).to render_template(:edit)
+      end
+
+      it 'redirects to profile path when current_user != params[username]' do
+        sign_out @user
+        sign_in @other
+        get :edit, params: { username: @user.username }
+        expect(response).to redirect_to(profile_path)
       end
     end
 
     context 'when user is NOT logged in' do
-      it 'responds succesfully' do 
+      it 'redirects to profile_path' do 
         get :edit, params: { username: @user.username }
-        expect(response).to redirect_to(:show)
+        expect(response).to redirect_to(profile_path)
       end
     end
   end
