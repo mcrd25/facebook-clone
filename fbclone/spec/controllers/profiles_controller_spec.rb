c25784a3e9f7f443e93fcecd7f259b0e716d18fa
Adriaan Beiertz <adriaanbd@gmail.com>
2019-06-28 10:11:43 -0500

    let instead of before, and added tests for current user viewing own profile vs another profile

--- fbclone/spec/controllers/profiles_controller_spec.rb
+++ fbclone/spec/controllers/profiles_controller_spec.rb
@@ -2,75 +2,147 @@ require 'rails_helper'
 
 RSpec.describe ProfilesController, type: :controller do
 
-  before do 
-        @user = FactoryBot.create(:user)
-        @other = FactoryBot.create(:user)
-      end
+  let(:user) { FactoryBot.create(:user) }
+  let(:other) { FactoryBot.create(:user) }
 
   describe 'GET show' do 
-    
 
-    context 'when logged user is logged in' do 
+    context 'when user is logged in' do 
 
-      it 'responds succesfully' do 
-        sign_in @user
-        get :show, params: { username: @user.username }
-        expect(response).to be_successful
+      before do 
+        sign_in user
       end
 
-      it 'responds with 200' do
-        sign_in @user
-        get :show, params: { username: @user.username }
-        expect(response).to have_http_status '200'
+      context 'when viewing his own profile' do 
+        it 'responds succesfully' do 
+          get :show, params: { username: user.username }
+          expect(response).to be_successful
+        end
+
+        it 'responds with 200' do
+          get :show, params: { username: user.username }
+          expect(response).to have_http_status '200'
+        end
+
+        it 'does not respond with 404' do
+          get :show, params: { username: user.username }
+          expect(response).to_not have_http_status '404'
+        end
+
+        it 'renders :show template' do
+          get :show, params: { username: user.username }
+          expect(response).to render_template(:show)
+        end 
       end
 
-      it 'does not respond with 404' do
-        sign_in @user
-        get :show, params: { username: @user.username }
-        expect(response).to_not have_http_status '404'
+      context 'when viewing another profile' do
+        it 'responds succesfully' do 
+          get :show, params: { username: other.username }
+          expect(response).to be_successful
+        end
+
+        it 'responds with 200' do
+          get :show, params: { username: other.username }
+          expect(response).to have_http_status '200'
+        end
+
+        it 'does not respond with 404' do
+          get :show, params: { username: other.username }
+          expect(response).to_not have_http_status '404'
+        end
+
+        it 'renders :index template' do
+          get :show, params: { username: other.username }
+          expect(response).to render_template(:show)
+        end
+      end
+    end
+
+    context 'when user is NOT logged in' do 
+      context 'when viewing his own profile' do 
+        it 'responds succesfully' do 
+          get :show, params: { username: user.username }
+          expect(response).to be_successful
+        end
+
+        it 'responds with 200' do
+          get :show, params: { username: user.username }
+          expect(response).to have_http_status '200'
+        end
+
+        it 'does not respond with 404' do
+          get :show, params: { username: user.username }
+          expect(response).to_not have_http_status '404'
+        end
+
+        it 'renders :show template' do
+          get :show, params: { username: user.username }
+          expect(response).to render_template(:show)
+        end 
       end
 
-      it 'renders index' do
-        sign_in @user 
-        get :show, params: { username: @user.username }
-        expect(response).to render_template(:show)
-      end 
+      context 'when viewing another profile' do
+        it 'responds succesfully' do 
+          get :show, params: { username: other.username }
+          expect(response).to be_successful
+        end
+
+        it 'responds with 200' do
+          get :show, params: { username: other.username }
+          expect(response).to have_http_status '200'
+        end
+
+        it 'does not respond with 404' do
+          get :show, params: { username: other.username }
+          expect(response).to_not have_http_status '404'
+        end
+
+        it 'renders :show template' do
+          get :show, params: { username: other.username }
+          expect(response).to render_template(:show)
+        end
+      end
     end
   end
 
   describe 'GET edit' do 
+
     context 'when user is logged in' do
 
       before do 
-        sign_in @user
+        sign_in user
       end
 
-      it 'responds succesfully' do 
-        get :edit, params: { username: @user.username }
-        expect(response).to be_successful
+      context 'when editing his own profile' do 
+        it 'responds succesfully' do 
+          get :edit, params: { username: user.username }
+          expect(response).to be_successful
+        end
+
+        it 'responds with 200' do 
+          get :edit, params: { username: user.username }
+          expect(response).to have_http_status '200'
+        end
+
+        it 'renders :edit template' do 
+          get :edit, params: { username: user.username }
+          expect(response).to render_template(:edit)
+        end
       end
 
-      it 'responds succesfully' do 
-        get :edit, params: { username: @user.username }
-        expect(response).to have_http_status '200'
-      end
-
-      it 'renders :edit when current_user == params[username]' do 
-        get :edit, params: { username: @user.username }
-        expect(response).to render_template(:edit)
-      end
-
-      it 'redirects to profile path when current_user != params[username]' do
-        sign_out @user
-        sign_in @other
-        get :edit, params: { username: @user.username }
-        expect(response).to redirect_to(profile_path)
+      context 'when editing another profile' do
+        it 'redirects to profile path' do
+          sign_out user
+          sign_in other
+          get :edit, params: { username: user.username }
+          expect(response).to redirect_to(profile_path)
+        end
       end
     end
 
     context 'when user is NOT logged in' do
       it 'redirects to profile_path' do 
-        get :edit, params: { username: @user.username }
+        get :edit, params: { username: user.username }
         expect(response).to redirect_to(profile_path)
       end
     end
