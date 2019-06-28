427997a586aeef05a82f71b14207d793eea8cede
mcrd25 <mayacrd25@gmail.com>
2019-06-28 13:08:03 -0400

    Added custom routes for login, logout and signup but it has issues

--- fbclone/config/routes.rb
+++ fbclone/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , path_names: { sign_in: 'login',
+                                             sign_out: 'logout',
+                                             sign_up: 'reg' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :profile, only: [:show, :edit],
                      path: '/:username'
