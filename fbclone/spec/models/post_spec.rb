a85e2423057580d241fcb92ff3ef01c799ba4bd9
mcrd25 <mayacrd25@gmail.com>
2019-07-08 12:52:52 -0400

    Removed unnecessary comments, added user_id tests to notification model spec and remove Model Contraints test for future refactoring.

--- fbclone/spec/models/post_spec.rb
+++ fbclone/spec/models/post_spec.rb
@@ -16,10 +16,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-	let!(:post) { FactoryBot.create(:post) }
-
-  let(:ilegal_id) { User.count.nil? ? 1 : User.count + 1 }
-  let(:ilegal_post) { Post.new(user_id: ilegal_id, message: "Hello World") }
+	let(:post) { FactoryBot.create(:post) }
 
 	describe 'test for presence of model attributes' do
 		context 'general expected attributes' do
@@ -118,12 +115,6 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  describe 'Constraints' do 
-    context 'when post is created with user that does not exist' do 
-      it 'should raise user must exist error' do 
-        expect { ilegal_post.save! }.to  raise_error(ActiveRecord::RecordInvalid, 'Validation failed: User must exist')
-      end 
-    end 
-  end
+  
 
 end
