61330d4a4099d75cca2b8f7e2c11f8825d68e68b
mcrd25 <mayacrd25@gmail.com>
2019-07-16 18:29:28 -0400

    Amended application helper to cater for no classs passed

--- fbclone/app/helpers/application_helper.rb
+++ fbclone/app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  def gravatar_for(user, classes)
+  def gravatar_for(user, classes = nil)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.full_name, class: "gravatar #{classes}")
