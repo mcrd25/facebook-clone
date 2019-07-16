54f648ea07203576cd3558f0997c901f0ab572d7
mcrd25 <mayacrd25@gmail.com>
2019-07-16 15:02:39 -0400

    Completed REceivd requests functionality and layout. Delete request and confirm friendship fucntionality

--- fbclone/app/controllers/friendships_controller.rb
+++ fbclone/app/controllers/friendships_controller.rb
@@ -2,7 +2,13 @@ class FriendshipsController < ApplicationController
   def create
     if user_signed_in?
       @friendship = Friendship.new(active_friend_id: params[:active_friend_id], passive_friend_id: current_user.id)
-      @friendship.save
+      if @friendship.save
+        @request = FriendRequest.where(["requester_id = ? and requestee_id = ?", params[:active_friend_id], current_user.id]).first
+        @request.destroy
+        redirect_to requests_received_requests_path
+      else
+        redirect_to root_path
+      end
     end
   end
 
--- fbclone/app/controllers/requests/received_requests_controller.rb
+++ fbclone/app/controllers/requests/received_requests_controller.rb
@@ -2,6 +2,6 @@ class Requests::ReceivedRequestsController < ApplicationController
 
 	def index
 		redirect_to(root_path) if not_signed_in? 
-		@requests = current_user.received_requests if user_signed_in?
+		@requesters = current_user.received_requests if user_signed_in?
 	end
 end
