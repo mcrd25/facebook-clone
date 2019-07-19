1606104f474c4bb4d0f14a410fcdf6372260d026
Adriaan Beiertz <adriaanbd@gmail.com>
2019-07-19 18:02:23 -0500

    home view for posts with comment-uncomment and like-unlike functionality

--- fbclone/app/controllers/comments_controller.rb
+++ fbclone/app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
       @comment.user_id = current_user.id
       @comment.post_id = params[:post_id]
       if @comment.save
+        if session[:source] == 'home'
+          redirect_to root_path
+        else
+          redirect_to profile_posts_path
+        end
+
         # appropriate flash message should be rendered
       else
         redirect_to profile_posts_path
