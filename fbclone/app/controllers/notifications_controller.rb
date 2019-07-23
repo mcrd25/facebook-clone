1a8b778333f1041aa9b7738d26c56d9d7b57c868
mcrd25 <mayacrd25@gmail.com>
2019-07-23 13:43:27 -0400

    Added notification and received requests badge functionality

--- fbclone/app/controllers/notifications_controller.rb
+++ fbclone/app/controllers/notifications_controller.rb
@@ -2,6 +2,15 @@ class NotificationsController < ApplicationController
 
 	def index
 		redirect_to(root_path) if not_signed_in? 
-		@notifications = current_user.notifications.order(created_at: :desc) if user_signed_in?
+		if user_signed_in?
+			@notifications = current_user.notifications.order(created_at: :desc) 
+			set_to_read(@notifications)
+		end
+	end
+
+	private
+
+	def set_to_read(notifs)
+		notifs.where(['status = ? AND user_id = ?', 'Unread', current_user]).update(status: 'Read')
 	end
 end
